ed3d6e204f952b3a99fdafe9a4d2cf98775b1a75
Robson Barros <robsonfullstackweb@gmail.com>
2023-03-15 20:33:30 -0300

    formulario pegarndo respostas e armazenando no array respostas

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
 session_start();
-$perguntas = ['Qual seu nome?','Qual sua idade?'];
-
+$perguntas = ['Qual seu nome?','Qual sua idade?','qual sua cor favorita'];
+if (!isset($_SESSION['respostas'])) 
+	// code.
 $_SESSION['respostas'] = array();
 
 if (isset($_POST['count'])) {
-	$_SESSION['respostas'][$_POST['count']] = $_POST['respostas'];
+	$_SESSION['respostas'][$_POST['count']] = @$_POST['resposta'];
 	if (count($perguntas) == $_POST['count'] + 1) {
-		die('exame finalizado');
+		header('location: resultado.php');
 		
 	}
 }
@@ -20,9 +21,9 @@ $index = isset($_POST['count']) ? (int)$_POST['count'] + 1 : 0;
 <form method="POST">
 	
 
-<h2><?php echo $perguntas[$index];?></h2>
-<input type="text" name="resposta e ir para proxima pergunta" >
-<input type="submit" name="acao" value="enviar">
+<h2><?php echo $perguntas[$index]?></h2>
+<input type="text" name="resposta">
+<input type="submit" name="acao" value="enviar resposta">
 <input type="hidden" name="count" value="<?php echo $index; ?>">
 
 </form>
