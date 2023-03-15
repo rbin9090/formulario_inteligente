f73e2210beb93404907cae1baedd813e87b0b661
Robson Barros <robsonfullstackweb@gmail.com>
2023-03-15 19:40:22 -0300

    adicionado perguntas

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ session_start();
 $perguntas = ['Qual seu nome?','Qual sua idade?'];
 
 $_SESSION['respostas'] = array();
+
 if (isset($_POST['count'])) {
 	$_SESSION['respostas'][$_POST['count']] = $_POST['respostas'];
-	if (count($perguntas)) {
-		// code...
+	if (count($perguntas) == $_POST['count'] + 1) {
+		die('exame finalizado');
+		
 	}
 }
 $index = isset($_POST['count']) ? (int)$_POST['count'] + 1 : 0;
@@ -15,12 +17,12 @@ $index = isset($_POST['count']) ? (int)$_POST['count'] + 1 : 0;
 
 
 
-<form>
+<form method="POST">
 	
 
 <h2><?php echo $perguntas[$index];?></h2>
 <input type="text" name="resposta e ir para proxima pergunta" >
 <input type="submit" name="acao" value="enviar">
-<input type="hidden" name="cont" value="<?php echo $index; ?>">
+<input type="hidden" name="count" value="<?php echo $index; ?>">
 
 </form>
