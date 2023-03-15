90a97d3bd43e1f8207f6b79bb4f2e702dc506294
Robson Barros <robsonfullstackweb@gmail.com>
2023-03-15 19:03:10 -0300

    adicionado arrays

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ session_start();
 $perguntas = ['Qual seu nome?','Qual sua idade?'];
 
 $_SESSION['respostas'] = array();
-
-$index = isset($_POST['count']) ? (int)$_POST['count'] : 0;
+if (isset($_POST['count'])) {
+	$_SESSION['respostas'][$_POST['count']] = $_POST['respostas'];
+	if (count($perguntas)) {
+		// code...
+	}
+}
+$index = isset($_POST['count']) ? (int)$_POST['count'] + 1 : 0;
 ?>
 
 
@@ -14,5 +19,8 @@ $index = isset($_POST['count']) ? (int)$_POST['count'] : 0;
 	
 
 <h2><?php echo $perguntas[$index];?></h2>
+<input type="text" name="resposta e ir para proxima pergunta" >
+<input type="submit" name="acao" value="enviar">
+<input type="hidden" name="cont" value="<?php echo $index; ?>">
 
 </form>
